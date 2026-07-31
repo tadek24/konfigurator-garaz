a6daf6f68e7e3969b124da63a47d27246ba89caa
tadek24 <tadekkw123@gmail.com>
2026-07-31 23:00:04 +0200

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
         console.error("Błąd dekodowania BIM:", e); 
       }
     }
-  }, [appData]);
+  }, []);
 
   const handleScroll = (e: UIEvent<HTMLDivElement>) => {
     const target = e.currentTarget;
